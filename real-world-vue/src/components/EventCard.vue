c35fd0f43a5b3e9c77c6a4e80ebaaaa7b29aa61d
Arthur <arthur4456645@gmail.com>
2024-04-24 15:03:53 +0800

    git hub

---
+++ real-world-vue/src/components/EventCard.vue
@@ -0,0 +1,40 @@
+<script setup>
+
+defineProps({
+  event: {
+    type: Object,
+    required: true,
+  },
+})
+
+</script>
+
+<template>
+  <div class="event-card">
+    <h2>{{ event.title }}</h2>
+    <span>@{{ event.time }} on {{ event.date }}</span>
+  </div>
+</template>
+
+<style scoped>
+  .event-card {
+    min-height: 130px;
+    padding: 20px;
+    width: 250px;
+    cursor: pointer;
+    border: 1px solid #39495c;
+    margin-bottom: 18px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center
+  }
+  .event-card:hover {
+    transform: scale(1.01);
+    box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+  }
+  h2{
+    margin: 0 0 0.5rem 0
+  }
+
+</style>
